bef610d0bb862977ccd6a29091c82d5d02f9d01f
kushwaha-abhi <166372666+kushwaha-abhi@users.noreply.github.com>
2024-12-21 20:06:49 +0530

    Created the download csv backend routes

--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authMiddleware = require("../middleware/auth.middleware");
 const roomController = require("../controllers/room.controller");
 const upload = require("../middleware/multer")("upload");
+const download = require("../controllers/csv")
 // const uploadMiddleware = require("./uploadMiddleware");
 router.put('/checkOut',roomController.checkoutRoom );
 router.post("/add", roomController.createRoom);
@@ -11,6 +12,6 @@ router.put("/cancel/:roomNumber", roomController.cancelRoom);
 router.post("/bookroom", upload.single("image"), roomController.bookRoom);
 router.get("/getall", roomController.getAllRooms);
 router.get("/bookings", roomController.getAllBookings);
-
+router.get("/csvFile", download.downloadFile)
 module.exports = router;
 
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,6 +3,7 @@ const router= express.Router();
 const {body}= require("express-validator");
 const userController = require("../controllers/user.controller");
 const authMiddleware= require("../middleware/auth.middleware")
+
 router.post('/register',[
     body("email").isEmail().withMessage("Invalid email"),
     body("password").isLength({min:5}).withMessage("password should be minimum 6 figures")
